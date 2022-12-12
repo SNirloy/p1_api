300194db1568f842b331805957c32cd2dad73fd1
Thomas <tzhang12062004@gmail.com>
2022-12-12 13:17:35 -0500

    .

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,30 @@
+"""
+Cold Embers: Sadi Nirloy, Emily Ortiz, Gabriel Thompson, Thomas Zhang
+Software Development
+p1: API Project ft. Database
+2022-12-11
+Time Spent: 
+"""
+
 from flask import Flask, session, render_template, request
-import sqlite3
 import utl.table_handler as table_handler
-import utl.api_handler as api_handler
 
 app = Flask(__name__)
+app.secret_key = os.urandom(32)
 
-@app.route("/")
+table_handler.setup() # setup tables
+
+@app.route("/", methods = ["POST", "GET"])
 def landing():
-    return render_template('landing.html')
+    if not 'username' in session:
+        #if ()
+    else:
+        return render_template('landing.html')
+
+@app.route("/registrate")
+def registration()
+    return render_template(landing.html)
+
 
 if __name__ == "__main__":
     app.debug = True
