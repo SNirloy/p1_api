a85532eb0cdc14bf7611f6c052f58841997b5779
Thomas <tzhang12062004@gmail.com>
2022-12-12 23:43:45 -0500

    pain

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,8 @@ p1: API Project ft. Database
 Time Spent: 
 """
 
-from flask import Flask, session, render_template, request
+from flask import Flask, session, render_template, request, redirect
+import os
 import utl.table_handler as table_handler
 
 app = Flask(__name__)
@@ -17,14 +18,29 @@ table_handler.setup() # setup tables
 @app.route("/", methods = ["POST", "GET"])
 def landing():
     if not 'username' in session:
-        #if ()
+        return render_template('login.html')
     else:
         return render_template('landing.html')
 
-@app.route("/registrate")
-def registration()
-    return render_template(landing.html)
+@app.route("/register", methods = ['GET', 'POST'])
+def register_page():
+    return render_template('register.html')
 
+@app.route("/registrate", methods = ['GET', 'POST'])
+def registration():
+    table_handler.registrate(request.form["createusername"], request.form["createpassword"])
+    return redirect("/")
+
+@app.route("/login", methods = ['GET', 'POST'])
+def login():
+    if (table_handler.user_check(request.form["username"]) == True):
+        if (table_handler.password_check(request.form["username"], request.form["password"]) == True):
+            session['username'] = request.form['username']
+            return redirect('/')
+        else:
+            return redirect("/")
+    else: 
+        return redirect("/")
 
 if __name__ == "__main__":
     app.debug = True
