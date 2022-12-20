8ef05326140749d0d5b8c724df1f7d088db031b2
unknown <eortiz30@stuy.edu>
2022-12-19 21:18:59 -0500

    changes

--- app/utl/api_handler.py
+++ app/utl/api_handler.py
@@ -2,7 +2,7 @@ import requests
 import json
 import os
 
-# takes in valid 4 letter ICAO or 3 letter IATA
+# takes in valid 3 letter IATA
 # returns latitude and longitude in an array
 def airport_api(airport_code):
     url = "https://airport-info.p.rapidapi.com/airport"
@@ -32,7 +32,7 @@ def airport_api(airport_code):
 
     # response is a dict of what the API returns
     response = requests.get(url, headers=headers, params=querystring).json()
-    print(response)
+    # print(response)
 
     output = [response["latitude"], response["longitude"]]
     return(output)
@@ -74,7 +74,7 @@ def yelp_api(location):
 
     response = requests.get(url, headers=headers).json()
     # print(json.dumps(response, indent=2))
-    # print(response["businesses"])
+    print(response["businesses"])
     
     output = []
 
@@ -92,16 +92,18 @@ def yelp_api(location):
         main_info["url"] = all_info["url"]
         main_info["price"] = all_info["price"]
         main_info["rating"] = all_info["rating"]
-        # distance
+        main_info["distance"] = all_info["distance"]
         # desc
+        # latitude
+        # longitude
     return output   
 
-# takes in array of data [latitude, longitude, start, end]
+# takes in array of data [latitude, longitude, start, end] yyyy-mm-dd
 def booking_api(input):
-    latitude = location[0]
-    longitude = location[1]
-    start = location[2]
-    end = location[3]
+    latitude = input[0]
+    longitude = input[1]
+    start = input[2]
+    end = input[3]
 
     path = os.path.dirname(os.path.realpath(__file__)) # path to current python file
     key = open(path + "/../keys/key_rapid.txt", "r").read()
@@ -109,7 +111,7 @@ def booking_api(input):
 
     url = "https://booking-com.p.rapidapi.com/v1/hotels/search-by-coordinates"
 
-    querystring = {"locale":"en-gb",\
+    querystring = {"locale":"en-us",\
     "filter_by_currency":"USD",\
     "order_by":"popularity",\
     "latitude":latitude,\
@@ -147,21 +149,16 @@ def booking_api(input):
     # print(json.dumps(output, indent=2))
     return output
 
-# print("==================== valid_airport_code test ====================")
-# print("should be False, False, True, True")
-# print(valid_airport_code("AAAA")) # False ICAO
-# print(valid_airport_code("LKS")) # False IATA
-# print(valid_airport_code("KJFK")) # True ICAO
-# print(valid_airport_code("JFK")) # True IATA
 
 
 # print("==================== airport_api test ====================")
 # print("should be [33.94159, -118.40853]")
-# # print(airport_api("KLAX"))
 # print(airport_api("LAX"))
 # print("==================== yelp_api test ====================")
 coords = airport_api("LAX")
-results = yelp_api(coords)
-print(json.dumps(results, indent=2))
+# results = yelp_api(coords)
+# print(json.dumps(results, indent=2))
+coords.append("2022-12-20")
+coords.append("2033-12-21")
 results = booking_api(coords)
 print(json.dumps(results, indent=2))
