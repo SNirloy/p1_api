9599ffd9e1d573b98110f79c40bfa58be3fc5bc5
unknown <eortiz30@stuy.edu>
2022-12-19 22:20:52 -0500

    airport and yelp should be good

--- app/utl/api_handler.py
+++ app/utl/api_handler.py
@@ -4,6 +4,9 @@ import os
 
 # takes in valid 3 letter IATA
 # returns latitude and longitude in an array
+# [latitude, longitude]
+# returns a string if the latitude is not found in the API response (most likely b/c of a bad key)
+# "An error occured. Try checking your API key."
 def airport_api(airport_code):
     url = "https://airport-info.p.rapidapi.com/airport"
 
@@ -11,42 +14,43 @@ def airport_api(airport_code):
     # print(path)
     key = open(path + "/../keys/key_rapid.txt", "r").read()
     key = key.strip()
+    # key = "invalid key test"
     # print(key)
 
-    querystring = {}
     headers = {
         "X-RapidAPI-Key": key,
         "X-RapidAPI-Host": "airport-info.p.rapidapi.com"
     }
 
-    # if len(airport_code) == 4: # 4 letter ICAO code
-    #     querystring["icao"] = airport_code
-    #     # print("ICAO code added to querystring")
-    # else: # 3 letter IATA code
-    #     querystring["iata"] = airport_code
-    #     # print("IATA code added to querystring")
-
-    querystring["iata"] = airport_code
-
+    querystring = {"iata": airport_code}
     # print("querystring: " + str(querystring))
 
     # response is a dict of what the API returns
     response = requests.get(url, headers=headers, params=querystring).json()
     # print(response)
 
-    output = [response["latitude"], response["longitude"]]
+    if "latitude" not in response.keys():
+        output = "An error occured. Try checking your API key."
+    else:
+        output = [response["latitude"], response["longitude"]]
     return(output)
 
+
 # takes in array of numbers [latitude, longitude]
-# returns array of businesses and restaurants within a 5 mile radius of the location
+# returns array of 5 (can try diff numbers later) businesses and restaurants within a 5 mile radius of the input location
 # each element in the array is a dictionary for one business containing the following info
-# - name (string)
-# - display_address (array where each element is an address line, each element is a string)
-# - display_phone (string)
-# - url (string)
-# - price (string)
-# - Hours NEED TO DO THIS
-# - rating (float)
+# {
+#     "name": "Benny's Tacos & Rotisserie Chicken - Westchester",
+#     "display_address": "7101 W Manchester Ave",
+#     "url": "https://www.yelp.com/biz/bennys-tacos-and-rotisserie-chicken-westchester-los-angeles?adjust_creative=LUk6VM2I0dwxuj2MZku81w&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=LUk6VM2I0dwxuj2MZku81w",
+#     "price": "$$",
+#     "rating": 4.5,
+#     "distance": 1.4,
+#     "latitude": 33.95998,
+#     "longitude": -118.41685
+#   }
+# returns # returns a string if the key "businesses" is not found in the API response
+# "An error occured. Try checking your API key."
 def yelp_api(location):
     # print(location)
     latitude = location[0]
@@ -58,13 +62,13 @@ def yelp_api(location):
     f"&longitude={longitude}" +\
     "&radius=8000" +\
     "&sort_by=best_match" +\
-    "&limit=5" # +\
-    # "&term=restaurant"
+    "&limit=5"
 
     path = os.path.dirname(os.path.realpath(__file__)) # path to current python file
     # print(path)
     key = open(path + "/../keys/key_yelp.txt", "r").read()
     key = key.strip()
+    # key = "invalid key test"
     # print(key)
 
     headers = {
@@ -74,29 +78,36 @@ def yelp_api(location):
 
     response = requests.get(url, headers=headers).json()
     # print(json.dumps(response, indent=2))
-    print(response["businesses"])
+    
+    # error message/string in case something happens
+    if "businesses" not in response.keys():
+        return "An error occured. Try checking your API key."
+
+    # array of businesses, each element is a dictionary with one businesses' info
+    businesses = response["businesses"] 
+    # print(json.dumps(businesses, indent=2))
+
+    num_businesses = len(businesses) # same as limit in url
+    # print("number of businesses/restaurants: " + str(num_businesses))
     
     output = []
-
-    businesses = response["businesses"] # array of businesses, each element is a dictionary with one businesses' info
-    num_businesses = len(businesses) # same as limit in url
-    print("number of businesses/restaurants: " + str(num_businesses))
-    
     for i in range(num_businesses):
         all_info = businesses[i] # dict for one business
         output.append({})
         main_info = output[i] # dict for one business
         main_info["name"] = all_info["name"]
-        main_info["display_address"] = all_info["location"]["display_address"]
-        # main_info["display_phone"] = all_info["display_phone"]
+        main_info["display_address"] = all_info["location"]["display_address"][0]
         main_info["url"] = all_info["url"]
         main_info["price"] = all_info["price"]
         main_info["rating"] = all_info["rating"]
-        main_info["distance"] = all_info["distance"]
-        # desc
-        # latitude
-        # longitude
+        # distance is in miles, rounded to the nearest 10th
+        # distance is distance btwn two points, not driving distance
+        main_info["distance"] = round(all_info["distance"] * 0.000621371, 1)
+        main_info["latitude"] = all_info["coordinates"]["latitude"]
+        main_info["longitude"] = all_info["coordinates"]["longitude"]
+        # where do i get a description from?
     return output   
+
 
 # takes in array of data [latitude, longitude, start, end] yyyy-mm-dd
 def booking_api(input):
@@ -154,11 +165,14 @@ def booking_api(input):
 # print("==================== airport_api test ====================")
 # print("should be [33.94159, -118.40853]")
 # print(airport_api("LAX"))
+
 # print("==================== yelp_api test ====================")
-coords = airport_api("LAX")
-# results = yelp_api(coords)
+# coords = airport_api("LAX")
+# yelp_results = yelp_api(coords)
+# print(yelp_results)
+# print(json.dumps(yelp_results, indent=2))
+
+# coords.append("2022-12-20")
+# coords.append("2033-12-21")
+# results = booking_api(coords)
 # print(json.dumps(results, indent=2))
-coords.append("2022-12-20")
-coords.append("2033-12-21")
-results = booking_api(coords)
-print(json.dumps(results, indent=2))
