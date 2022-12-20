6e5b90c56a3e966fb74163103aad0cef6f8ed83a
unknown <eortiz30@stuy.edu>
2022-12-20 00:34:14 -0500

    so proud of myself, things have been going good wth, no breakdowns yet :face_holding_back_tears:

--- app/utl/api_handler.py
+++ app/utl/api_handler.py
@@ -5,7 +5,8 @@ import os
 # takes in valid 3 letter IATA
 # returns latitude and longitude in an array
 # [latitude, longitude]
-# returns a string if the latitude is not found in the API response (most likely b/c of a bad key)
+
+# returns a string if the key "latitude" is not found in the API response (most likely b/c of a bad key)
 # "An error occured. Try checking your API key."
 def airport_api(airport_code):
     url = "https://airport-info.p.rapidapi.com/airport"
@@ -49,7 +50,8 @@ def airport_api(airport_code):
 #     "latitude": 33.95998,
 #     "longitude": -118.41685
 #   }
-# returns # returns a string if the key "businesses" is not found in the API response
+
+# returns a string if the key "businesses" is not found in the API response
 # "An error occured. Try checking your API key."
 def yelp_api(location):
     # print(location)
@@ -109,7 +111,25 @@ def yelp_api(location):
     return output   
 
 
-# takes in array of data [latitude, longitude, start, end] yyyy-mm-dd
+# takes in array of data [latitude, longitude, start date, end date] 
+# dates are in yyyy-mm-dd
+# currently looks for 1 room for 2 adults, change this in future?
+# locale and currency are en-us and USD, have not tested inputting foreign airports coords with these parameters
+# returns array of up to 5 (can try diff numbers later) hotels, array has 5 hotels majority of the time, not sure what search radius is
+# each element in the array is a dictionary for one business containing the following info
+# {
+# "hotel_name": "Super 8 by Wyndham Oneida Verona",
+# "address": "215 Genesse Street",
+# "min_total_price": 152.31,
+# "latitude": 43.0788,
+# "longitude": -75.653398
+# }
+
+# returns a string if the key "result" is not found in the API response
+# "An error occured. Try checking your API key. Additionally, make sure your dates have not already passed, your dates are within 30 days of each other, and your start date is before your end date."
+
+# returns a string if no hotels are found (will probably never run into this though because airports are bound to have hotels nearby)
+# "No results were found. Try a different location."
 def booking_api(input):
     latitude = input[0]
     longitude = input[1]
@@ -119,6 +139,7 @@ def booking_api(input):
     path = os.path.dirname(os.path.realpath(__file__)) # path to current python file
     key = open(path + "/../keys/key_rapid.txt", "r").read()
     key = key.strip()
+    # key = "invalid key test"
 
     url = "https://booking-com.p.rapidapi.com/v1/hotels/search-by-coordinates"
 
@@ -139,38 +160,54 @@ def booking_api(input):
     }
 
     response = requests.request("GET", url, headers=headers, params=querystring).json()
+    # print(response)
+
+    # response if invalid API key:
+    # {'message': 'You are not subscribed to this API.'}
+    if "result" not in response.keys():
+        return "An error occured. Try checking your API key. Additionally, make sure your dates have not already passed, your dates are within 30 days of each other, and your start date is before your end date."
 
     hotels = response["result"] # array of hotels
-    # print(len(hotels))
-    print(json.dumps(hotels[0], indent=2))
-    # print(hotels[0]["hotel_name"])
-    # print(hotels[0]["address"])
+    num_hotels = len(hotels)
+    # print("number of results: " + str(num_hotels))
+
+    if num_hotels == 0:
+        return "No results were found. Try a different location."
+    
+    # print("info for one hotel:")
+    # print(json.dumps(hotels[0], indent=2))
+
+    # if statement should work, haven't been able to test for a location with < 5 results though
+    if num_hotels < 5:
+        num_hotels_returned = num_hotels
+    else:
+        num_hotels_returned = 5
+    
+    # print("number of hotels to return: " + str(num_hotels_returned))
 
     output = []
-    for i in range(5):
+    for i in range(num_hotels_returned):
         hotel = hotels[i]
         temp = {}
         temp["hotel_name"] = hotel["hotel_name"]
         temp["address"] = hotel["address"]
-        temp["min_total_price"] = hotel["min_total_price"]
+        temp["min_total_price"] = round(hotel["min_total_price"], 2)
         temp["latitude"] = hotel["latitude"]
         temp["longitude"] = hotel["longitude"]
         output.append(temp)
+    return output  
 
-    # print(json.dumps(output, indent=2))
-    return output
-
-   def map_api(coordinates):
-        return f"""
-		<div>
- 			<iframe width="500" height="400" frameborder="0" src="https://www.bing.com/maps/embed?h=400&w=500&cp={coordinates[0]}~{coordinates[1]}&lvl=13&typ=d&sty=r&src=SHELL&FORM=MBEDV8" scrolling="no">
-			</iframe>
-			<div style="white-space: nowrap; text-align: center; width: 500px; padding: 6px 0;">
-				<a id="largeMapLink" target="_blank" href="https://www.bing.com/maps?cp={coordinates[0]}~{coordinates[1]}&amp;sty=r&amp;lvl=13&amp;FORM=MBEDLD">View Larger Map</a> &nbsp; | &nbsp;
-				<a id="dirMapLink" target="_blank" href="https://www.bing.com/maps/directions?cp={coordinates[0]}~{coordinates[1]}&amp;sty=r&amp;lvl=13&amp;rtp=~pos.{coordinates[0]}_{coordinates[1]}____&amp;FORM=MBEDLD">Get Directions</a>
-			</div>
-		</div>
-	"""
+def map_api(coordinates):
+    return f"""
+    <div>
+        <iframe width="500" height="400" frameborder="0" src="https://www.bing.com/maps/embed?h=400&w=500&cp={coordinates[0]}~{coordinates[1]}&lvl=13&typ=d&sty=r&src=SHELL&FORM=MBEDV8" scrolling="no">
+        </iframe>
+        <div style="white-space: nowrap; text-align: center; width: 500px; padding: 6px 0;">
+            <a id="largeMapLink" target="_blank" href="https://www.bing.com/maps?cp={coordinates[0]}~{coordinates[1]}&amp;sty=r&amp;lvl=13&amp;FORM=MBEDLD">View Larger Map</a> &nbsp; | &nbsp;
+            <a id="dirMapLink" target="_blank" href="https://www.bing.com/maps/directions?cp={coordinates[0]}~{coordinates[1]}&amp;sty=r&amp;lvl=13&amp;rtp=~pos.{coordinates[0]}_{coordinates[1]}____&amp;FORM=MBEDLD">Get Directions</a>
+        </div>
+    </div>
+"""
 
 # print("==================== airport_api test ====================")
 # print("should be [33.94159, -118.40853]")
@@ -179,10 +216,39 @@ def booking_api(input):
 # print("==================== yelp_api test ====================")
 # coords = airport_api("LAX")
 # yelp_results = yelp_api(coords)
-# print(yelp_results)
+# # print(yelp_results)
 # print(json.dumps(yelp_results, indent=2))
 
-# coords.append("2022-12-20")
-# coords.append("2033-12-21")
-# results = booking_api(coords)
+# print("==================== booking_api test ====================")
+# coords = airport_api("LAX")
+# data = coords
+# data.append("2022-12-30") # start date
+# data.append("2022-12-31") # end date
+# results = booking_api(data)
 # print(json.dumps(results, indent=2))
+
+# print("South Pole Test:")
+# data = [90, 45, "2022-12-30", "2022-12-31"]
+# results = booking_api(data)
+# print(results)
+
+# print("Small Town Test (Sherrill, NY):")
+# print("Trying to make sure booking_api() works when there's < 5 results from the API but even this small town has 15 results")
+# data = [43.0737, -75.5982, "2022-12-30", "2022-12-31"]
+# results = booking_api(data)
+# print(json.dumps(results, indent=2))
+
+# print("Dates Too Far Apart Test")
+# data = [90, 45, "2023-01-01", "2023-12-31"]
+# results = booking_api(data)
+# print(results)
+
+# print("Dates Already Passed Test")
+# data = [90, 45, "2021-01-01", "2021-12-31"]
+# results = booking_api(data)
+# print(results)
+
+# print("Checkout Before Checkin Test")
+# data = [90, 45, "2022-12-31", "2022-12-30"]
+# results = booking_api(data)
+# print(results)
